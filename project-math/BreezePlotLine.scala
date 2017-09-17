e2a1dafead89033f1bb2a384aa6304f81a2753ac
staticor <staticor0828@gmail.com>
2017-09-17 17:15:29 +0800

    chapter math over, add plotting code

---
+++ project-math/BreezePlotLine.scala
@@ -0,0 +1,24 @@
+package linalg.plot
+import breeze.linalg._
+import breeze.plot._
+
+object BreezePlotLine {
+  def main(args: Array[String]): Unit = {
+
+    val f = Figure()
+    val p = f.subplot(0)
+    val x = DenseVector(0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8)
+    val y = DenseVector(1.1, 2.1, 0.5, 1.0, 3.0, 1.1, 0.0, 0.5,2.5)
+    p += plot(x,  y)
+    p.xlabel = "x axis"
+    p.ylabel = "y axis"
+    f.saveas("lines-graph.png")
+
+    val f2 = Figure()
+    val p2 = f2.subplot(2, 1, 1)
+    val g2 = breeze.stats.distributions.Gaussian(0, 1)
+    p2 += hist(g2.sample(100000), 100)
+    p2.title = "A normal distribution Histogram"
+    f2.saveas("Gaussian distribution.png")
+  }
+}
